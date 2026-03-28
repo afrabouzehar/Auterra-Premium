ac6b9d937aaf469506b0070dddb4df25024fd91b
afrabouzehar <bouzehar.afra@gmail.com>
2026-03-28 08:37:12 +0100

    fixing javasript

--- js/catalogue.js
+++ js/catalogue.js
@@ -1,28 +1,25 @@
 
-  
-   document.querySelectorAll('.btn-filtre').forEach(btn => {
-    btn.classList.remove('actif');
-  });
-  
-  bouton.classList.add('actif');
-  const cartes = document.querySelectorAll('.carte-voiture');
-  
-  cartes.forEach(carte => (
-    const carteCategorie = carte.getAttribute('data-categorie');
-    
-    if (categorie === 'tous') {
-      
-      carte.classList.remove('cache');
-      console.log(`Carte ${carteCategorie} : affichée`);
-    } else if (carteCategorie === categorie) {
-      
-      carte.classList.remove('cache');
-      console.log(`Carte ${carteCategorie} : affichée`);
-    } else {
-      
-      carte.classList.add('cache');
-      console.log(`Carte ${carteCategorie} : cachée`);
-    }
-  });
-}
+document.querySelectorAll('.btn-filtre').forEach(btn => {
+  btn.classList.remove('actif');
+});
 
+bouton.classList.add('actif');
+
+const cartes = document.querySelectorAll('.carte-voiture');
+
+cartes.forEach(carte => {
+  const carteCategorie = carte.getAttribute('data-categorie');
+
+  if (categorie === 'tous') {
+    carte.classList.remove('cache');
+    console.log(`Carte ${carteCategorie} : affichée`);
+  } 
+  else if (carteCategorie === categorie) {
+    carte.classList.remove('cache');
+    console.log(`Carte ${carteCategorie} : affichée`);
+  } 
+  else {
+    carte.classList.add('cache');
+    console.log(`Carte ${carteCategorie} : cachée`);
+  }
+});
